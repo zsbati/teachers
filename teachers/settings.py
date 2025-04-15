0a527a49fdc3386745c967cc9006f0f2a1594439
zsbati <zbati123@gmail.com>
2025-04-15 20:24:00 +0100

    favicon, background, logo fine.

--- teachers/settings.py
+++ teachers/settings.py
@@ -2,6 +2,7 @@
 Django settings for teachers project.
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -95,10 +96,11 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
